8dff43937c4b10a75a5139754f88f1ff6785cd6b
Jendlet <t.jendle@hotmail.com>
2025-03-08 17:23:39 +0100

    started with a hard draft of what to be included in GA and genome

---
+++ geneticAlgorithm.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+class GeneticAlgorithm():
+    def __init__(self,orders,beds,gaParams):
+        self.orders = orders
+        self.beds = beds
+        self.gaParams = gaParams
+    
+    def encode_genomes():
+        pass
+    def decode_genomes():
+        pass
+    def crossover_genomes():
+        pass
+    def get_fittest_genome():
+        pass
+    def do_generation():
+        pass
+    def generate_population():
+        pass
---
+++ genome.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Genome():
+    def __init__(self,orders,beds,production):
+        self.dna
+    def encode_ponton(self,bed_id,bed_x,bed_y,start_time):
+        '''Encodes a specific ponton, will be part of the dna array
+        
+            Returns
+            ---
+            bit array'''
+        pass
+    def encode(self):
+        '''Encodes all pontons, a '''
+        pass
+    def decode(self):
+        pass
+    def mutate(self):
+        pass
+    def validate(self):
+        pass
