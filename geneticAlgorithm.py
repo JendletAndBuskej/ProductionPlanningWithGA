826462ef23699cd158467098c810c369d17a5199
FLEXLINK\SETJE0 <theodor.jendle@flexlink.com>
2025-03-12 16:39:11 +0100

    did some work on GA and Gen class

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -1,20 +1,47 @@
 import numpy as np 
+from gen import Gen
+# from order import Order
+# from bed import Bed
 
 class GeneticAlgorithm():
-    def __init__(self,orders,beds,gaParams):
+    def __init__(self,
+            orders: list[int], beds: list[int], bed_nbr_slots_x: int, bed_nbr_slots_y: int,
+            nbr_time_slots: int, population_count: int, mutation_multiplier: int=1.
+        ):
         self.orders = orders
         self.beds = beds
-        self.gaParams = gaParams
+        self.bed_nbr_slots_x = bed_nbr_slots_x
+        self.bed_nbr_slots_y = bed_nbr_slots_y
+        self.nbr_time_slots = nbr_time_slots
+        self.population_count = population_count
+        self.mutation_multiplier = mutation_multiplier
+        self.fittest_individual = None
     
-    def encode_genomes():
-        pass
-    def decode_genomes():
-        pass
-    def crossover_genomes():
-        pass
-    def get_fittest_genome():
+    def generate_population(self):
+        self.population = np.empty([self.population_count,],dtype=object)
+        for iGen in range(self.population_count):
+            gen = Gen(
+                self.orders, self.beds, self.bed_nbr_slots_x,
+                self.bed_nbr_slots_x, self.nbr_time_slots
+            )
+            # extract data from initial schedule and mak
+            gen.randomize_gen()
+            self.population[iGen] = gen
+    
+    def mutate(self, gen: Gen):
+        gen_length = np.shape(gen)[0]
+        mutation_rate = self.mutation_multiplier/float(gen_length)
+        for genome in gen:
+            r = np.random.uniform(0, 1)
+            if r > mutation_rate:
+                continue
+            genome = 1 if genome == 0 else 0
+        return self
+    
+    def crossover(self, gen_father: Gen, gen_mother: Gen):
         pass
     def do_generation():
         pass
-    def generate_population():
-        pass
+
+GA = GeneticAlgorithm([3,2],[1,2],2,2,3,60)
+GA.generate_population()
