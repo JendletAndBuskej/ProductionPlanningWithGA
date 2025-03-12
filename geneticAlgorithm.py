8258756b30b84818d473fd3df948575d3076f9fe
FLEXLINK\SETJE0 <theodor.jendle@flexlink.com>
2025-03-12 17:26:14 +0100

    asd

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -1,5 +1,6 @@
 import numpy as np 
 from gen import Gen
+import copy
 # from order import Order
 # from bed import Bed
 
@@ -27,6 +28,8 @@ class GeneticAlgorithm():
             # extract data from initial schedule and mak
             gen.randomize_gen()
             self.population[iGen] = gen
+        self.genomes_per_gen = np.shape(self.population[0].gen)[0]
+        return self
     
     def mutate(self, gen: Gen):
         gen_length = np.shape(gen)[0]
@@ -38,10 +41,29 @@ class GeneticAlgorithm():
             genome = 1 if genome == 0 else 0
         return self
     
-    def crossover(self, gen_father: Gen, gen_mother: Gen):
+    def get_parents(self):
         pass
+
+    def crossover_uniform(self, gen_father: Gen, gen_mother: Gen):
+        gen_son = copy.deepcopy(gen_father)
+        gen_daughter = copy.deepcopy(gen_mother)
+        for iGenome in range(self.genomes_per_gen):
+            r = np.random.uniform(0, 1)
+            if r > .5:
+                genome_son = gen_son.gen[iGenome]
+                gen_son.gen[iGenome] = gen_daughter.gen[iGenome]
+                gen_daughter.gen[iGenome] = genome_son
+        return self
+    
+    def crossover_k_point(self, gen_father: Gen, gen_mother: Gen, k_points: int):
+        gen_son = copy.deepcopy(gen_father)
+        gen_daughter = copy.deepcopy(gen_mother)
+        random_points = np.random.choice(np.arange(0,k_points),size=self.genomes_per_gen,replace=False)
+        points = np.sort(random_points)
+        
     def do_generation():
         pass
 
 GA = GeneticAlgorithm([3,2],[1,2],2,2,3,60)
-GA.generate_population()
+# GA.generate_population().crossover_uniform(GA.population[0],GA.population[1])
+GA.generate_population().crossover_k_point(GA.population[0],GA.population[1],70)
